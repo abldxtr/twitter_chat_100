4314055ff0af1e6d1d91d61df8dcbe2309ea2912
abldxtr <110703641+abldxtr@users.noreply.github.com>
2024-11-06 15:04:27 +0000

    unstable cache

--- lib/data.ts
+++ lib/data.ts
@@ -1,11 +1,28 @@
 import db from "./prisma";
+import { unstable_cache } from "@/lib/unstable-cache";
 
-export async function fetchChat() {
-  try {
+
+export const fetchChat = unstable_cache(
+  async () => {
     const data = await db.user.findMany();
 
+  
+
     return data;
-  } catch (error) {
-    console.log("fetchChat error");
-  }
-}
+  },
+  ["fetchChat"],
+  { tags: ["fetchChat"] }
+);
+
+
+// export async function fetchChat() {
+//   try {
+//     const data = await db.user.findMany();
+
+//     return data;
+//   } catch (error) {
+//     console.log("fetchChat error");
+//   }
+// }
+
+
